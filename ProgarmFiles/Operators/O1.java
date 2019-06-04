65bbd513f7fa5b310a1c090cb61ca890665cd938
visalrotanak <45762799+visalrotanak@users.noreply.github.com>
2019-06-04 19:36:50 +0700

    Add files via upload

---
+++ ProgarmFiles/Operators/O1.java
@@ -0,0 +1,31 @@
+class O1{
+	public static void main(String[] args){
+	/*	int a=10, b=2;
+		System.out.println("a+b = " + (a+b));
+    		System.out.println("a-b = " + (a-b));
+    		System.out.println("a*b = " + (a*b));
+    		System.out.println("a/b = " + (a/b));
+		System.out.println((a+b) +" = a+b");
+	*/
+	/*	System.out.println("3.0/2 = "+ 3.0/2);
+		System.out.println("Answer : " + (3/2.0) +" "+ (4.5-0.5));
+	*/
+	/*	 int x = 10, y = 3;
+		double z = 4.5;
+		System.out.println(x/y);
+		System.out.println((double)x/y);
+		System.out.println((int)z);
+	*/
+	/*	 int a=10,b=10,c=10,d=10;
+  		System.out.println("value of a++ = " +(a++));
+  		System.out.println("a = "+a);
+  		System.out.println("value of ++b = " +(++b));
+  		System.out.println("value of c-- = " +(c--));
+  		System.out.println("value of --d = " +(--d));
+	*/
+		//System.out.println(Math.abs(-2.3)); //Result: 2.3
+		//System.out.println(Math.ceil(3.4)); //Result: 4.0
+		System.out.println((int)Math.pow(5,3)); //Result: 125
+		//System.out.println()
+	}
+}
