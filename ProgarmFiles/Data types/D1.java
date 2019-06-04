a0c7b0872f80fb14c0e55de348ff9cf9edadd53e
visalrotanak <45762799+visalrotanak@users.noreply.github.com>
2019-06-04 19:35:20 +0700

    Add files via upload

---
+++ ProgarmFiles/Data types/D1.java
@@ -0,0 +1,12 @@
+class D1{
+	public static void main(String[] args){
+		/* double b = 123.4567;
+		char c = 'e';
+		boolean d = true;
+		System.out.println("Double :"+ b);
+		System.out.println("Character :"+ c);
+		System.out.println("Boolean :"+ d); */
+		
+		
+	}
+}
