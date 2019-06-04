8467ca3a0a9c62251760e68bdd85bc9264999d36
visalrotanak <45762799+visalrotanak@users.noreply.github.com>
2019-06-04 19:37:21 +0700

    Add files via upload

---
+++ ProgarmFiles/Take Input/I1.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class I1{
+	public static void main(String[] agrs){
+	/*	Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter an integer");
+		int a;
+		a = s1.nextInt();
+		System.out.println("The enterd integer is " + a);
+	*/
+	/*	Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter integer");
+		int n = s1.nextInt();  //s1 is object of Scanner class
+
+		System.out.println("Enter Double");
+		double db = s1.nextDouble();
+
+		System.out.println("Integer: " + n);
+		System.out.println("Double: " + db);
+	*/
+	/*	Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter your first name");
+		String f = s1.next();
+		System.out.println("Enter your last name");
+		String l = s1.next();
+		System.out.println("Your name is "+f+" "+l);
+	*/	
+	/*	Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name = s1.nextLine();
+		System.out.println("Your name is " + name);
+	*/
+	/*	Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String name = s1.next();
+		System.out.println("Your name is "+name);
+		System.out.println("Enter your name again");
+		String name1 = s1.nextLine();
+		System.out.println("Your name is "+name1);
+	*/	//Here, next took only the first word. Rest of the words were taken by nextLine.
+	}
+}
